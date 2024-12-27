8a96fc0e96aa839974638cce9bc965bcb6d6e4fd
PMCLSF <pmclachlansf@gmail.com>
2024-12-26 22:09:25 -0800

    chore: update test index to include all test files

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -1,15 +1,52 @@
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import * as glob from 'glob';
 
-// Core
-import "./core/parser.test";
-import "./core/astHelper.test";
-import "./core/converter.test";
+export function run(): Promise<void> {
+ const mocha = new Mocha({
+   ui: 'bdd',
+   color: true
+ });
 
-// Features
-import "./features/settings.test";
-import "./features/highlighting.test";
+ const testsRoot = path.resolve(__dirname);
 
-// Integration
-import "./integration/concurrency.test";
-import "./integration/security.test";
-import "./integration/e2e.test";
+ return new Promise((resolve, reject) => {
+   glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+     if (err) {
+       return reject(err);
+     }
 
+     // Add test files
+     files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+     try {
+       mocha.run(failures => {
+         if (failures > 0) {
+           reject(new Error(`${failures} tests failed.`));
+         } else {
+           resolve();
+         }
+       });
+     } catch (err) {
+       reject(err);
+     }
+   });
+ });
+}
+
+// Import all test files
+import './extension.test';
+
+// Core tests
+import './core/parser.test';
+import './core/astHelper.test';
+import './core/converter.test';
+
+// Feature tests  
+import './features/settings.test';
+import './features/highlighting.test';
+
+// Integration tests
+import './integration/concurrency.test';
+import './integration/security.test';
+import './integration/e2e.test';
