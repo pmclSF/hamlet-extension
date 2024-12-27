8d2cf8f732205e8f2be1c65ccfb146ec4523a98b
PMCLSF <pmclachlansf@gmail.com>
2024-12-26 22:03:20 -0800

    refactor: organize test files into core/features/integration directories

---
+++ test/suite/index.ts
@@ -0,0 +1,15 @@
+
+// Core
+import "./core/parser.test";
+import "./core/astHelper.test";
+import "./core/converter.test";
+
+// Features
+import "./features/settings.test";
+import "./features/highlighting.test";
+
+// Integration
+import "./integration/concurrency.test";
+import "./integration/security.test";
+import "./integration/e2e.test";
+
