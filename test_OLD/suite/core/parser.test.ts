55ac3d3f2b51c3e63bd0f5c6b9c83bf802192669
PMCLSF <pmclachlansf@gmail.com>
2024-12-29 17:29:48 -0800

    remove files that should be ignored

--- test_OLD/suite/core/parser.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('Parser Tests', () => {
-    it('should run a basic test', () => {
-        expect(true).to.equal(true);
-    });
-});
--- test_OLD/suite/extension.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
-
-describe('Extension Test Suite', () => {
-    it('Sample test', () => {
-        console.log('Running sample test');
-        expect(true).to.be.true;
-    });
-});
