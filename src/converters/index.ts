f08bb0093e79a1831f92c5f90cc74a5b0b0c58f2
PMCLSF <pmclachlansf@gmail.com>
2025-01-03 10:28:59 -0800

    Fixing syntax error in .gitignore.

--- src/converters/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './converter';
-export * from './cypress/to-playwright';
-export * from './playwright/to-testrail';
-export * from './testrail/to-cypress';
-export * from './testrail/to-playwright';
